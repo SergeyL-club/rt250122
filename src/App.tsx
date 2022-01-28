00aa835e56dc06190e552e8b495e2b3ee36eedd3
SergeyL-club <damnazzel@gmail.com>
2022-01-28 12:01:16 +0500

    clear all

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
-import { useState } from "react";
-import AuthForm from "./pages/AuthForm";
-import Header from "./pages/Header";
-
 function App() {
-  const [isAuthForm, setIsAuthForm] = useState(false);
-
-  function openAuthForm() {
-    setIsAuthForm(true);
-  }
-  function closeAuthForm() {
-    setIsAuthForm(false);
-  }
-
-  return (
-    <>
-      {isAuthForm ? <AuthForm closeForm={closeAuthForm} /> : null}
-      <Header openAuthForm={openAuthForm} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,12 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
-import UserProvider from "./context/user.context";
 import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route
-        path="/"
-        element={
-          <UserProvider>
-            <App />
-          </UserProvider>
-        }
-      />
+      <Route path="/" element={<App />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
